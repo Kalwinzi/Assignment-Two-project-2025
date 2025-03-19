40700d65e588317b7b19bde63fda18e957d49970
Kalwinzi masuluzu chapa <kalwinzic@gmail.com>
2025-03-19 17:43:55 +0300

    UserReg.js
    User registration

---
+++ UserReg.js
@@ -0,0 +1,14 @@
+// controllers/authController.js
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword, role });
+    res.status(201).json({ success: true, data: user });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+};
