46fcc6021535eef57fa5db0f82510e7449a33017
Kalwinzi masuluzu chapa <kalwinzic@gmail.com>
2025-03-21 09:56:47 +0300

    Create InputValidation.js
    Input validation

---
+++ InputValidation.js
@@ -0,0 +1,15 @@
+// middleware/validationMiddleware.js
+const { body, validationResult } = require('express-validator');
+
+exports.validateUserRegistration = [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('email').isEmail().withMessage('Invalid email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
+    }
+    next();
+  },
+];
